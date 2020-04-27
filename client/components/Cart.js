046dd13598fe2e1efe418232e3b6ca93982f0ac3
nurpny <runnurrr@gmail.com>
2020-04-27 16:49:38 -0400

    Style Cart view

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -17,9 +17,14 @@ const StyledCartContainer = styled(StyledContainer)`
     min-width: 60%;
     max-width: 100%;
     margin: 10px 25px 10px 20px;
-    display: flex;
-    flex-direction: column;
+    display: grid;
+    grid-column: 1fr;
 `
+const StyledCartTitle = styled.section`
+  font-family: ${props => props.theme.fonts.bookTitleFt};
+  font-size: 1.0rem;
+`
+
 const StyledAmountContainer = styled(StyledContainer)`
     min-width: 30%;
     max-width: 100%;
@@ -27,8 +32,10 @@ const StyledAmountContainer = styled(StyledContainer)`
 `
 
 const StyledItemContainer = styled.section`
-  display: flex;
-  flex-direction: row;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 10px;
+  margin-top: 10px;
 `
 const StyledBookImg = styled.img`
   height: 150px;
@@ -43,18 +50,22 @@ export class Cart extends Component {
     return (
       <StyledFlexContainer>
         <StyledCartContainer>
-          <div>Shopping Cart</div>
+          <StyledCartTitle>Shopping Cart</StyledCartTitle>
           {this.props.cart.map(cartItem =>
             < StyledItemContainer key={cartItem.bookId} >
               <StyledBookImg src={cartItem.book.bookImageURL} />
               <div>{cartItem.book.title}</div>
-              <div>{cartItem.quantity}</div>
-              <div>{cartItem.price/100}</div>
+              <div>x {cartItem.quantity}</div>
+              <div>$ {cartItem.price/100}</div>
             </ StyledItemContainer>
           )}
 
         </StyledCartContainer>
         <StyledAmountContainer>
+          <StyledCartTitle>Subtotal:
+            $ {this.props.cart ? this.props.cart.reduce((acc, item) => acc += item.quantity * item.price, 0)/100: $0}
+          </StyledCartTitle>
+
 
         </StyledAmountContainer>
 
--- server/routes/cart.js
+++ server/routes/cart.js
@@ -24,7 +24,7 @@ router.get('/', async (req, res, next) => {
           dateCompleted: null
         }
       })
-    } let cart = {}
+    } let cart = []
     if (order) {
       cart = await Cart.findAll({
         where: {
